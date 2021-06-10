f6bd8b2171d6d4a502ca2e40257db71ef2dfafa5
Taehyun Kim <kgyoo8232@gmail.com>
2021-06-10 22:37:06 +0900

    fix onChangeText

--- src/InputExample.tsx
+++ src/InputExample.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export function InputExample () {
+export function InputExample() {
     const [year, setYear] = useState(1990);
     const [month, setMonth] = useState(1);
     const [date, setDate] = useState(1);
@@ -9,45 +9,45 @@ export function InputExample () {
         portal: 'google',
     });
 
-    const {id, portal} = inputs;
-    
+    const { id, portal } = inputs;
+
     const onIncreaseYear = () => {
-        setYear (prev => prev+1);
-    }    
+        setYear(prev => prev + 1);
+    }
     const onIncreaseMonth = () => {
-        setMonth (prev => prev+1);
+        setMonth(prev => prev + 1);
     }
     const onIncreaseDate = () => {
-        setDate (prev => prev+1);
+        setDate(prev => prev + 1);
     }
 
     const onDecreaseYear = () => {
-        setYear (prev => prev-1);
+        setYear(prev => prev - 1);
     }
     const onDecreaseMonth = () => {
-        setMonth (prev => prev-1);
+        setMonth(prev => prev - 1);
     }
     const onDecreaseDate = () => {
-        setDate (prev => prev-1);
+        setDate(prev => prev - 1);
     }
 
     const onChangeYear = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if(!isNaN(+e.target.value)) {
+        if (!isNaN(+e.target.value)) {
             setYear(+e.target.value)
         }
     }
     const onChangeMonth = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if(!isNaN(+e.target.value)) {
+        if (!isNaN(+e.target.value)) {
             setMonth(+e.target.value)
         }
     }
     const onChangeDate = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if(!isNaN(+e.target.value)) {
+        if (!isNaN(+e.target.value)) {
             setDate(+e.target.value)
         }
     }
-    const onChangeText = (e: { target: { id: any; value: any; }; }) => {
-        const {value, id} = e.target;
+    const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value, id } = e.target;
 
         setInputs({
             ...inputs,
@@ -57,7 +57,7 @@ export function InputExample () {
 
     return (
         <div>
-            <br/><br/><br/>
+            <br /><br /><br />
             <div>
                 <h1>{year}년 {month}월{date}일</h1>
             </div>
@@ -66,50 +66,52 @@ export function InputExample () {
                 <h2>{id}@{portal}.com</h2>
             </div>
 
-            <br/><br/>
-            <input 
-                className='InputExampleInputBox' 
+            <br /><br />
+            <input
+                className='InputExampleInputBox'
                 value={year}
                 onChange={onChangeYear}
             />
             <button onClick={onIncreaseYear}>+1</button>
-            <button onClick={onDecreaseYear}>-1</button>            
-            <br/>
+            <button onClick={onDecreaseYear}>-1</button>
+            <br />
 
-            <input 
-                className='InputExampleInputBox' 
+            <input
+                className='InputExampleInputBox'
                 value={month}
                 onChange={onChangeMonth}
             />
             <button onClick={onIncreaseMonth}>+1</button>
             <button onClick={onDecreaseMonth}>-1</button>
-            <br/>
+            <br />
 
-            <input 
+            <input
                 className='InputExampleInputBox'
                 value={date}
                 onChange={onChangeDate}
             />
             <button onClick={onIncreaseDate}>+1</button>
             <button onClick={onDecreaseDate}>-1</button>
-            <br/><br/>
+            <br /><br />
 
-            <input 
+            <input
                 className='InputExampleInputBox'
                 value={id}
+                id='id'
                 name='id'
                 placeholder='id'
                 onChange={onChangeText}
             />
-            <input 
+            <input
                 className='InputExampleInputBox'
                 value={portal}
+                id='portal'
                 name='portal'
                 placeholder='portal'
                 onChange={onChangeText}
             />
-            
-            <br/><br/><br/><br/><br/><br/>
+
+            <br /><br /><br /><br /><br /><br />
         </div>
     )
 }
