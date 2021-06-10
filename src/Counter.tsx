b12fb0a041b729435a8130d214a579d7f343518e
HanSeungHyeop <h970409@gmail.com>
2021-06-10 14:18:02 +0900

    test

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -22,6 +22,7 @@ export function Counter() {
             <h1>{number}</h1>
             <button onClick={onIncrease}>+1</button>
             <button onClick={onDecrease}>-1</button>
+            <br/>
             <input onChange={onChange} value={number}/>
         </div>
     )
